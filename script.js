bbf63d51cdb9755d690fa05e662eca92c01cec90
Julian Jelfs <julian.jelfs@gmail.com>
2014-02-23 21:33:44 +0000

    grid drawing with bomb position and numbers around bombs

--- script.js
+++ script.js
@@ -1,29 +1,82 @@
 
 (function(){
 
+    function surroundingCells(cells, row, col){
+        var near = [];
+        if(cells[(row-1)+'_'+(col-1)]){
+            near.push(cells[(row-1)+'_'+(col-1)]);
+        }
+        if(cells[(row-1)+'_'+(col)]){
+            near.push(cells[(row-1)+'_'+(col)]);
+        }
+        if(cells[(row-1)+'_'+(col+1)]){
+            near.push(cells[(row-1)+'_'+(col+1)]);
+        }
+        if(cells[(row)+'_'+(col-1)]){
+            near.push(cells[(row)+'_'+(col-1)]);
+        }
+        if(cells[(row)+'_'+(col+1)]){
+            near.push(cells[(row)+'_'+(col+1)]);
+        }
+        if(cells[(row+1)+'_'+(col-1)]){
+            near.push(cells[(row+1)+'_'+(col-1)]);
+        }
+        if(cells[(row+1)+'_'+(col)]){
+            near.push(cells[(row+1)+'_'+(col)]);
+        }
+        if(cells[(row+1)+'_'+(col+1)]){
+            near.push(cells[(row+1)+'_'+(col+1)]);
+        }
+        return near;
+    }
+
     function init(){
         var size = $('#size').val(),
             bombs = $('#bombs').val(),
             stage = $('#stage').empty(),
             dim = 400 / size,
-            bombCells = {};
+            bombCells = [],
+            cell = 0;
 
         while(bombs > 0){
-            var c = Math.floor(Math.random() * size*size);
+            var c = Math.floor(Math.random()*size*size);
             if(bombCells[c] == null){
                 bombs -= 1;
                 bombCells[c] = true;
-            } else {
-                console.log('duplicate detected');
             }
         }
 
-        for(var y=0; y<size; y++) {
-            for(var x=0; x<size; x++) {
+        var cells = [];
+        for(var row=0; row<size; row++) {
+            for(var col=0; col<size; col++) {
+                cells[row+'_'+col] = {
+                    row : row,
+                    col : col,
+                    bomb : bombCells[cell++],
+                    nearby : 0
+                };
+            }
+        }
+
+        for(var row= 0, cell=0; row<size; row++) {
+            for(var col=0; col<size; col++) {
+                var key = row+'_'+col;
+                if(!cells[key].bomb) {
+                    $.each(surroundingCells(cells,row,col), function(i, val){
+                        if(val.bomb){
+                            cells[key].nearby += 1;
+                        }
+                    });
+                }
                 $('<div class="cell"/>').width(dim).height(dim)
-                    .data({x:x,y:y})
-                    .text(bombCells[(x*y)]?'x':'')
-                    .appendTo(stage);
+                    .html(cells[key].bomb?'<span class="bomb">x</span>':(cells[key].nearby > 0 ? cells[key].nearby : ''))
+                    .appendTo(stage).click((function(k){
+                        return function(){
+                            if(cells[k].bomb){
+                                alert('Kaboom!');
+                            }
+                        };
+                    })(key));
             }
         }
     }
